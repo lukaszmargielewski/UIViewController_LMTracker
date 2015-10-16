7ef6300a6196cb84d4f256dd293408fcecb898dc
Lukasz Margielewski <lukasz.margielewski@gmail.com>
2015-10-16 15:07:55 +0200

    Initial Commit

---
+++ UIVCTrackingExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIVCTrackingExample
+//
+//  Created by Lukasz Margielewski on 16/10/15.
+//  Copyright © 2015 Lukasz Margielewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIVCTrackingExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIVCTrackingExample
+//
+//  Created by Lukasz Margielewski on 16/10/15.
+//  Copyright © 2015 Lukasz Margielewski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
